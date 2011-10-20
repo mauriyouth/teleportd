702d232eaaff8a3a65ad7498c4015212714b8780
mauriyouth <git@github.com:mauriyouth/teleportd.git>
2011-10-20 15:44:05 +0200

    I can see a google map

---
+++ teleportd/src/teleportd/com/droid/TeleportdActivity.java
@@ -0,0 +1,19 @@
+package teleportd.com.droid;
+
+import com.google.android.maps.MapActivity;
+import android.os.Bundle;
+
+public class TeleportdActivity extends MapActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
